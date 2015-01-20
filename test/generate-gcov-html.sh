5b7b63d1a84724c28234512394aa7c8edf96af4c
Rémi Coulom <remi.coulom@free.fr>
2015-01-20 13:42:31 +0100

    branch coverage in gcov

--- test/generate-gcov-html.sh
+++ test/generate-gcov-html.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
-lcov --directory ../compcmake/ninja_debug --capture --output-file all.info || exit 1
-lcov  --remove all.info '/usr/*' --output app.info || exit 1
-c++filt -n <app.info >demangled.info
+
+lcov --directory ../compcmake/ninja_debug\
+     --capture\
+     --rc lcov_branch_coverage=1\
+     --output-file all.info || exit 1
+
+lcov --remove all.info '/usr/*'\
+     --rc lcov_branch_coverage=1\
+     --output app.info || exit 1
+
 mkdir -p html
-genhtml --output-directory html demangled.info || exit 1
+
+cd ../src
+src_dir=`pwd`
+cd -
+
+genhtml --prefix "$src_dir"\
+        --demangle-cpp\
+        --rc genhtml_branch_coverage=1\
+        --output-directory html app.info || exit 1
+
 echo xdg-open html/index.html
