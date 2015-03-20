ef9fcee95d0235fc5536abe5d1954ba891941351
Rémi Coulom <Remi.Coulom@univ-lille3.fr>
2015-03-20 10:21:50 +0900

    minor additions before going to Unbalance

--- src/interpreter/io/utf8.cpp
+++ src/interpreter/io/utf8.cpp
@@ -2,13 +2,41 @@
 
 #include <iostream>
 
+/////////////////////////////////////////////////////////////////////////////
 std::string joedb::read_utf8_string(std::istream &in)
+/////////////////////////////////////////////////////////////////////////////
 {
  std::string result;
  return result;
 }
 
-void write_utf8_string(std::ostream &out, const std::string &s)
+/////////////////////////////////////////////////////////////////////////////
+void write_utf8_string(std::ostream &out,
+                       const std::string &s,
+                       bool ascii_only)
+/////////////////////////////////////////////////////////////////////////////
 {
- out << '"' << s << '"';
+ out << '"';
+
+ for (size_t i = 0; i < s.size(); i++)
+ {
+  const char c = s[i];
+
+  if (c == '"')
+   out << "\\\"";
+  else if (c == '\\')
+   out << "\\";
+  else if (c == '\n')
+   out << "\\n";
+  else if (c == '\\')
+   out << "\\";
+  else if (c == '\t')
+   out << "\\t";
+  else
+  {
+   out c;
+  }
+ }
+
+ out << '"';
 }
--- src/interpreter/io/utf8.h
+++ src/interpreter/io/utf8.h
@@ -7,7 +7,9 @@ namespace joedb
 {
  // Parse and write utf8 strings with C++ syntax
  std::string read_utf8_string(std::istream &in); 
- void write_utf8_string(std::ostream &out, const std::string &s);
+ void write_utf8_string(std::ostream &out,
+                        const std::string &s,
+                        bool ascii_only);
 }
 
 #endif
