403edc91b20e618c669f6e14ada56d22f674e48d
Rémi Coulom <Remi.Coulom@univ-lille3.fr>
2015-03-21 12:09:30 +0900

    favor g++ so that gcov is used for tests

--- compcmake/generate.sh
+++ compcmake/generate.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+favorite_compiler=g++
+compiler_option=""
+compiler_path=`which $favorite_compiler`
+if [ "$compiler_path" != "" ]; then
+ echo Found $favorite_compiler: $compiler_path
+ compiler_option="-DCMAKE_CXX_COMPILER=$favorite_compiler"
+else
+ echo $favorite_compiler not found
+fi
+echo compiler_option:$compiler_option
+
 function generate {
  mkdir -p $1
  cd $1
@@ -8,8 +19,8 @@ function generate {
  cd ..
 }
 
-generate ninja_release cmake -G Ninja -DCMAKE_BUILD_TYPE=Release
-generate ninja_debug cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug
+generate ninja_release cmake -G Ninja -DCMAKE_BUILD_TYPE=Release $compiler_option
+generate ninja_debug cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug $compiler_option
 
 if [ $OSTYPE == "cygwin" ]; then
 # generate mingw32 cmake -G Ninja -DCMAKE_CXX_COMPILER=i686-w64-mingw32-g++
