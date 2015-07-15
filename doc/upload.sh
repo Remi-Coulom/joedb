a28dc0596dc11521aed2c477ff0023d876004b46
Rémi Coulom <remi.coulom@free.fr>
2015-07-15 17:47:01 +0200

    upload.sh

--- doc/upload.sh
+++ doc/upload.sh
@@ -5,7 +5,5 @@ make html
 #cd build/html
 #lftp -e "cd joedb; mirror -e -R;quit" remi.coulom@ftpperso.free.fr
 
-cd build
-mv -v html joedb
-rsync -r --delete joedb rcoulom@www.remi-coulom.fr:web/www
-mv -v joedb html
+cd build/html
+rsync -v -r --delete . rcoulom@www.remi-coulom.fr:web/www/joedb
