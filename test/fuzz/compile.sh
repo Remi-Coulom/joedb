f614b7d9d595f94ab61056354fb6a1c97851939d
Rémi Coulom <remi.coulom@free.fr>
2016-12-10 15:47:07 +0100

    compile.sh: automatically compile libFuzzer

--- test/fuzz/compile.sh
+++ test/fuzz/compile.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
-../../../third_party/llvm-build/Release+Asserts/bin/clang++\
+
+# http://llvm.org/docs/LibFuzzer.html#versions
+if [ ! -d third_party ]; then
+ echo Installing clang ...
+ mkdir TMP_CLANG
+ cd TMP_CLANG
+ git clone https://chromium.googlesource.com/chromium/src/tools/clang
+ cd ..
+ TMP_CLANG/clang/scripts/update.py
+fi
+
+# http://llvm.org/docs/LibFuzzer.html#building
+if [ ! -f libFuzzer.a ]; then
+ echo Building libFuzzer.a ...
+ git clone https://chromium.googlesource.com/chromium/llvm-project/llvm/lib/Fuzzer
+ PATH=`pwd`/third_party/llvm-build/Release+Asserts/bin:$PATH
+ ./Fuzzer/build.sh
+fi
+
+./third_party/llvm-build/Release+Asserts/bin/clang++\
  -fsanitize-coverage=trace-pc-guard\
  -fsanitize=address\
  -std=c++11\
@@ -9,10 +28,9 @@
  ../../src/interpreter/abstract/Database.cpp\
  ../../src/interpreter/abstract/Listener.cpp\
  ../../src/interpreter/abstract/Multiplexer.cpp\
- ../../src/interpreter/abstract/Safe_Listener.cpp\
  ../../src/interpreter/abstract/Table.cpp\
  ../../src/journal/Generic_File.cpp\
  ../../src/journal/Journal_File.cpp\
  ../../src/journal/Stream_File.cpp\
- ../../../Fuzzer/libFuzzer.a\
+ ./libFuzzer.a\
  -o joedb_fuzz
