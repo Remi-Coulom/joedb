Remi-Coulom/joedb

commit 47365ec2fa44d59642be2d4545230d6a8ea522d5

Fixed potential json crash found by fuzzing.

src/interpreter/io/json.cpp
@@ -75,8 +75,13 @@ void joedb::write_json(std::ostream &out, const Readable &db, bool base64)
       break;
 
       case Type::type_id_t::reference:
-       out << reference_translation[type.get_table_id()]
-               [db.get_reference(table_id, record_id, field_id)];
+      {
+       record_id_t i = db.get_reference(table_id, record_id, field_id);
+       const std::vector<int64_t> &v = reference_translation[table_id];
+       if (i >= v.size())
+        i = 0;
+       out << v[i];
+      }
       break;
 
       case Type::type_id_t::string:

test/fuzz/joedbi_corpus/interpreter_test.joedbi
@@ -12,6 +12,9 @@ insert_into person 1 "Rémi" 2
 insert_into person 2 "Norbert" 2
 insert_into person 3 "Alexandre" 1
 dump
+sql
+json 1
+help
 
 create_table city
 create_table 123456
@@ -83,6 +86,18 @@ insert_vector toto 3 1
 insert_vector toto 5 1
 update toto 3 value "toto"
 update_vector toto 5 value 2 "kiki" "kuku"
+
+rename_table toto __toto__
+rename_table azersdf toto
+add_field toto __field__ string
+add_field azersdf field string
+drop_field azersdf field
+rename_field toto value __value__
+rename_field azersdf value valux
+rename_field toto valux valux
+insert_into azersdf 1
+insert_vector azersdf 1 1
+delete_from azersdf 1
 
 dump
 

commit a950ea6237f779d1f1e50302004f2acc9ce336e3

Fix json bug recently introduced in commit 47365ec2fa44d59642be2d4545230d6a8ea522d5

src/interpreter/io/json.cpp
@@ -77,7 +77,7 @@ void joedb::write_json(std::ostream &out, const Readable &db, bool base64)
       case Type::type_id_t::reference:
       {
        Record_Id i = db.get_reference(table_id, record_id, field_id);
-       const std::vector<int64_t> &v = reference_translation[table_id];
+       const std::vector<int64_t> &v = reference_translation[type.get_table_id()];
        if (i >= v.size())
         i = 0;
        out << v[i];

test/interpreter_test.dump
@@ -7,6 +7,8 @@ insert_into city 1
 update city 1 name "Paris"
 insert_into city 2
 update city 2 name "Lille"
+insert_into city 15
+update city 15 name "Pétaouchnok"
 insert_into person 1
 update person 1 name "Rémi"
 update person 1 city 2
@@ -15,7 +17,7 @@ update person 2 name "Norbert"
 update person 2 city 2
 insert_into person 3
 update person 3 name "Alexandre"
-update person 3 city 1
+update person 3 city 15
 drop_table city
 drop_table person
 create_table test

test/interpreter_test.joedbi
@@ -8,9 +8,10 @@ add_field person name string
 add_field person city references city
 insert_into city 1 "Paris"
 insert_into city 2 "Lille"
+insert_into city 15 "Pétaouchnok"
 insert_into person 1 "Rémi" 2
 insert_into person 2 "Norbert" 2
-insert_into person 3 "Alexandre" 1
+insert_into person 3 "Alexandre" 15
 dump
 sql
 json 1

test/interpreter_test.out
@@ -5,24 +5,27 @@ OK: add_field person name string
 OK: add_field person city references city
 OK: insert_into city 1 "Paris"
 OK: insert_into city 2 "Lille"
+OK: insert_into city 15 "Pétaouchnok"
 OK: insert_into person 1 "Rémi" 2
 OK: insert_into person 2 "Norbert" 2
-OK: insert_into person 3 "Alexandre" 1
+OK: insert_into person 3 "Alexandre" 15
 create_table city
 create_table person
 add_field city name string
 add_field person name string
 add_field person city references city
 insert_vector city 1 2
+insert_vector city 15 1
 update city 1 name "Paris"
 update city 2 name "Lille"
+update city 15 name "Pétaouchnok"
 insert_vector person 1 3
 update person 1 name "Rémi"
 update person 2 name "Norbert"
 update person 3 name "Alexandre"
 update person 1 city 2
 update person 2 city 2
-update person 3 city 1
+update person 3 city 15
 CREATE TABLE "city"("__id" INTEGER PRIMARY KEY);
 CREATE TABLE "person"("__id" INTEGER PRIMARY KEY);
 ALTER TABLE "city" ADD "name" TEXT;
@@ -30,8 +33,10 @@ ALTER TABLE "person" ADD "name" TEXT;
 ALTER TABLE "person" ADD "city" INTEGER REFERENCES "city";
 INSERT INTO "city"("__id") VALUES(1);
 INSERT INTO "city"("__id") VALUES(2);
+INSERT INTO "city"("__id") VALUES(15);
 UPDATE "city" SET "name" = X'5061726973' WHERE "__id" = 1;
 UPDATE "city" SET "name" = X'4c696c6c65' WHERE "__id" = 2;
+UPDATE "city" SET "name" = X'50c3a974616f7563686e6f6b' WHERE "__id" = 15;
 INSERT INTO "person"("__id") VALUES(1);
 INSERT INTO "person"("__id") VALUES(2);
 INSERT INTO "person"("__id") VALUES(3);
@@ -40,18 +45,18 @@ UPDATE "person" SET "name" = X'4e6f7262657274' WHERE "__id" = 2;
 UPDATE "person" SET "name" = X'416c6578616e647265' WHERE "__id" = 3;
 UPDATE "person" SET "city" = 2 WHERE "__id" = 1;
 UPDATE "person" SET "city" = 2 WHERE "__id" = 2;
-UPDATE "person" SET "city" = 1 WHERE "__id" = 3;
+UPDATE "person" SET "city" = 15 WHERE "__id" = 3;
 {
  "city":
  {
-  "__size": 2,
-  "name": ["UGFyaXM=","TGlsbGU="]
+  "__size": 3,
+  "name": ["UGFyaXM=","TGlsbGU=","UMOpdGFvdWNobm9r"]
  },
  "person":
  {
   "__size": 3,
   "name": ["UsOpbWk=","Tm9yYmVydA==","QWxleGFuZHJl"],
-  "city": [1,1,0]
+  "city": [1,1,2]
  }
 }
 

test/interpreter_test.sql
@@ -7,6 +7,8 @@ INSERT INTO "city"("__id") VALUES(1);
 UPDATE "city" SET "name" = X'5061726973' WHERE "__id" = 1;
 INSERT INTO "city"("__id") VALUES(2);
 UPDATE "city" SET "name" = X'4c696c6c65' WHERE "__id" = 2;
+INSERT INTO "city"("__id") VALUES(15);
+UPDATE "city" SET "name" = X'50c3a974616f7563686e6f6b' WHERE "__id" = 15;
 INSERT INTO "person"("__id") VALUES(1);
 UPDATE "person" SET "name" = X'52c3a96d69' WHERE "__id" = 1;
 UPDATE "person" SET "city" = 2 WHERE "__id" = 1;
@@ -15,7 +17,7 @@ UPDATE "person" SET "name" = X'4e6f7262657274' WHERE "__id" = 2;
 UPDATE "person" SET "city" = 2 WHERE "__id" = 2;
 INSERT INTO "person"("__id") VALUES(3);
 UPDATE "person" SET "name" = X'416c6578616e647265' WHERE "__id" = 3;
-UPDATE "person" SET "city" = 1 WHERE "__id" = 3;
+UPDATE "person" SET "city" = 15 WHERE "__id" = 3;
 DROP TABLE "city";
 DROP TABLE "person";
 CREATE TABLE "test"("__id" INTEGER PRIMARY KEY);

test/select_data.out
@@ -7,6 +7,8 @@ insert_into city 1
 update city 1 name "Paris"
 insert_into city 2
 update city 2 name "Lille"
+insert_into city 15
+update city 15 name "Pétaouchnok"
 insert_into person 1
 update person 1 name "Rémi"
 update person 1 city 2
@@ -15,7 +17,7 @@ update person 2 name "Norbert"
 update person 2 city 2
 insert_into person 3
 update person 3 name "Alexandre"
-update person 3 city 1
+update person 3 city 15
 drop_table city
 drop_table person
 create_table test