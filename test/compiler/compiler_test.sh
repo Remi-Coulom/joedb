42625ef3df348c3c44ca1e4463ef23e1dd21f4f4
Rémi Coulom <remi.coulom@free.fr>
2017-05-04 11:54:11 +0200

    Detect error: more than one index using the same field does not work.

--- test/compiler/compiler_test.sh
+++ test/compiler/compiler_test.sh
@@ -10,10 +10,12 @@ rm -rvf *.joedb compiler_test.out
 "$build_dir"/joedbi test.joedb <test.joedbi
 "$build_dir"/joedbi vector_hole.joedb <vector_hole.joedbi
 "$build_dir"/joedbi vector_delete.joedb <vector_delete.joedbi
+"$build_dir"/joedbi multi_index.joedb <multi_index.joedbi
 "$build_dir"/joedbc test.joedbi test.joedbc
 "$build_dir"/joedbc schema_v1.joedbi schema_v1.joedbc
 "$build_dir"/joedbc schema_v2.joedbi schema_v2.joedbc
 "$build_dir"/joedbc vector_test.joedbi vector_test.joedbc
+"$build_dir"/joedbc multi_index.joedbi multi_index.joedbc
 
 
 cd "$build_dir"
