cc654d342f29825d5ed235340bbb317f592c771f
Rémi Coulom <remi.coulom@free.fr>
2017-05-06 19:03:42 +0200

    minimal_runtime.cpp

---
+++ src/compiler/minimal_runtime.cpp
@@ -0,0 +1,6 @@
+// minimal set of files to be linked with a compiled database
+#include "../journal/File.cpp"
+#include "../journal/Generic_File.cpp"
+#include "../journal/Journal_File.cpp"
+#include "../journal/Readonly_Journal.cpp"
+#include "../journal/Stream_File.cpp"
