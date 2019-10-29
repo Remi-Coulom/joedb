aa51c5e0232473ddc00d9da2b43c075f17873881
Rémi Coulom <remi.coulom@gmail.com>
2019-10-29 16:25:12 +0100

    Upgraded Google Test from 1.8.0 to 1.10.0

--- compcmake/get_gtest.sh
+++ compcmake/get_gtest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rvf googletest* *.zip
-wget https://github.com/google/googletest/archive/release-1.8.0.zip
-unzip release-1.8.0.zip
+wget https://github.com/google/googletest/archive/release-1.10.0.zip
+unzip release-1.10.0.zip
