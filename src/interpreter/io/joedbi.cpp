8c537eb90e5624da6f9b7f2a8b73c70ca4c09a51
Rémi Coulom <remi.coulom@gmail.com>
2019-10-29 16:33:58 +0100

    joedbi: catch all exceptions, and display error.

--- src/interpreter/io/joedbi.cpp
+++ src/interpreter/io/joedbi.cpp
@@ -11,50 +11,58 @@
 int main(int argc, char **argv)
 /////////////////////////////////////////////////////////////////////////////
 {
- joedb::Database db;
-
- if (argc <= 1)
+ try
  {
-  joedb::Interpreter interpreter(db);
-  interpreter.main_loop(std::cin, std::cout);
- }
- else
- {
-  std::shared_ptr<joedb::File> file;
+  joedb::Database db;
 
-  try
+  if (argc <= 1)
   {
-   file = std::make_shared<joedb::File>
-   (
-    argv[1],
-    joedb::Open_Mode::write_existing_or_create_new
-   );
-  }
-  catch (const joedb::Exception &)
-  {
-   file = std::make_shared<joedb::File>
-   (
-    argv[1],
-    joedb::Open_Mode::read_existing
-   );
-  }
-
-  if (file->get_mode() == joedb::Open_Mode::read_existing)
-  {
-   joedb::Readonly_Journal journal(*file);
-   journal.replay_log(db);
-   joedb::Readonly_Interpreter interpreter(db);
+   joedb::Interpreter interpreter(db);
    interpreter.main_loop(std::cin, std::cout);
   }
   else
   {
-   joedb::Journal_File journal(*file);
-   journal.replay_log(db);
-   joedb::Readable_Multiplexer multiplexer(db);
-   multiplexer.add_writeable(journal);
-   joedb::Interpreter interpreter(multiplexer);
-   interpreter.main_loop(std::cin, std::cout);
+   std::shared_ptr<joedb::File> file;
+
+   try
+   {
+    file = std::make_shared<joedb::File>
+    (
+     argv[1],
+     joedb::Open_Mode::write_existing_or_create_new
+    );
+   }
+   catch (const joedb::Exception &)
+   {
+    file = std::make_shared<joedb::File>
+    (
+     argv[1],
+     joedb::Open_Mode::read_existing
+    );
+   }
+
+   if (file->get_mode() == joedb::Open_Mode::read_existing)
+   {
+    joedb::Readonly_Journal journal(*file);
+    journal.replay_log(db);
+    joedb::Readonly_Interpreter interpreter(db);
+    interpreter.main_loop(std::cin, std::cout);
+   }
+   else
+   {
+    joedb::Journal_File journal(*file);
+    journal.replay_log(db);
+    joedb::Readable_Multiplexer multiplexer(db);
+    multiplexer.add_writeable(journal);
+    joedb::Interpreter interpreter(multiplexer);
+    interpreter.main_loop(std::cin, std::cout);
+   }
   }
+ }
+ catch (const joedb::Exception &e)
+ {
+  std::cerr << "Error: " << e.what() << '\n';
+  return 1;
  }
 
  return 0;
