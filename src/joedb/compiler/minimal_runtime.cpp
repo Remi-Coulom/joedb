268e422d4f2a9bc99b67f4b552cb98272fd2c2ff
Rémi Coulom <remi.coulom@gmail.com>
2020-10-18 15:34:03 +0200

    File_Slice.cpp in minimal_runtime

--- src/joedb/compiler/minimal_runtime.cpp
+++ src/joedb/compiler/minimal_runtime.cpp
@@ -2,6 +2,7 @@
 #include "../Writable.cpp"
 #include "../compiler/minimal_runtime_io.cpp"
 #include "../journal/File.cpp"
+#include "../journal/File_Slice.cpp"
 #include "../journal/Generic_File.cpp"
 #include "../journal/Writable_Journal.cpp"
 #include "../journal/Readonly_Journal.cpp"
