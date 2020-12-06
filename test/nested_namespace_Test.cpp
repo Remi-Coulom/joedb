780f681254fa5a94ed8eeb944516d1927b7e45ab
Rémi coulom <remi.coulom@gmail.com>
2020-12-06 20:36:36 +0100

    test for namespace_write

--- test/nested_namespace_Test.cpp
+++ test/nested_namespace_Test.cpp
@@ -1,9 +1,10 @@
 #include "gtest/gtest.h"
-
 #include "joedb/compiler/nested_namespace.h"
 
+#include <sstream>
+
 /////////////////////////////////////////////////////////////////////////////
-TEST(nested_namespace, splitting)
+TEST(nested_namespace, split_namespace)
 /////////////////////////////////////////////////////////////////////////////
 {
  auto n = joedb::split_namespace("split::this::name");
@@ -12,4 +13,14 @@ TEST(nested_namespace, splitting)
  EXPECT_EQ("split", n[0]);
  EXPECT_EQ("this", n[1]);
  EXPECT_EQ("name", n[2]);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(nested_namespace, namespace_write)
+/////////////////////////////////////////////////////////////////////////////
+{
+ auto n = joedb::split_namespace("split::this::name");
+ std::ostringstream out;
+ joedb::namespace_write(out, n, "!");
+ EXPECT_EQ("split!this!name", out.str());
 }
