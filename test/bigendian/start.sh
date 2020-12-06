243d717895960420b25f3872016ee08d1c0ab5c9
Rémi coulom <remi.coulom@gmail.com>
2020-12-06 19:04:13 +0100

    update qemu's deprecated -redir option

--- test/bigendian/start.sh
+++ test/bigendian/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-system-ppc -bios ./openbios-ppc -hda debian-wheezy-powerpc.qcow2 -L bios -m 1G -g 1024x768x8 -redir tcp:2222::22
+qemu-system-ppc -bios ./openbios-ppc -hda debian-wheezy-powerpc.qcow2 -L bios -m 2047M -g 1024x768x8 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
