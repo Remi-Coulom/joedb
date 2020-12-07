f65fe6f2c7ab6da5a88bc485bcec444cba5e2961
Rémi Coulom <remi.coulom@gmail.com>
2020-12-07 15:42:06 +0100

    removed old unused embind test

--- test/embind/embind_tutorial.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#~/emsdk_portable/emsdk activate latest
-source ~/emsdk_portable/emsdk_env.sh
-emcc -I/usr/local/include -s ASYNCIFY=1 -Oz --bind -o embind_tutorial.js embind_tutorial.cpp ../../doc/source/tutorial/tutorial.cpp ../../../joedb/src/compiler/joedbc_lib.cpp
--- test/embind/quick_example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#~/emsdk_portable/emsdk activate latest
-source ~/emsdk_portable/emsdk_env.sh
-emcc -Oz --bind -o quick_example.js quick_example.cpp
