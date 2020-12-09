07a982ba8d192d2e07d1a94c86f343c962f57da9
Rémi Coulom <remi.coulom@gmail.com>
2020-12-09 18:02:55 +0100

    Readonly_Memory_File

--- test/Generic_File_Test.cpp
+++ test/Generic_File_Test.cpp
@@ -1,4 +1,5 @@
 #include "joedb/journal/Memory_File.h"
+#include "joedb/journal/Readonly_Memory_File.h"
 
 #include "gtest/gtest.h"
 
@@ -16,4 +17,16 @@ TEST(Generic_File, copy)
  copy.set_position(0);
 
  EXPECT_EQ(copy.read<uint64_t>(), magic);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(Generic_File, readonly_memory_file)
+/////////////////////////////////////////////////////////////////////////////
+{
+ const char data[] = {0, 1, 2, 3};
+ const size_t size = sizeof(data);
+
+ joedb::Readonly_Memory_File file(data, size);
+
+ EXPECT_EQ(file.read<uint32_t>(), 0x03020100UL);
 }
