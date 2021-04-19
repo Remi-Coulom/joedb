4e14d4536c0cb3506800d53ff7034dd4aa39bbf7
Rémi Coulom <remi.coulom@gmail.com>
2021-04-19 23:04:38 +0200

    Windows compile

--- src/joedb/concurrency/net.h
+++ src/joedb/concurrency/net.h
@@ -4,18 +4,25 @@
 #if JOEDB_HAS_NET
 #include <net>
 namespace net = std::net;
+
 #elif JOEDB_HAS_EXPERIMENTAL_NET
 #include <experimental/net>
 namespace net = std::experimental::net;
+
 #elif JOEDB_HAS_BOOST_NET
 #ifdef _WIN32
 #include <windows.h>
 #endif
 #include <boost/asio/ts/net.hpp>
 namespace net = boost::asio;
+
 #elif JOEDB_HAS_ASIO_NET
+#ifdef _WIN32
+#include <windows.h>
+#endif
 #include <asio/ts/net.hpp>
 namespace net = asio;
+
 #else
 #error No networking
 #endif
