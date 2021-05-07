6a8ad38a37b3a26661a28f11a8c56ee54462d8f4
Rémi Coulom <remi.coulom@gmail.com>
2021-05-07 18:02:50 +0200

    test of Raw_Dump_Writable

--- test/Interpreter_Test.cpp
+++ test/Interpreter_Test.cpp
@@ -1,6 +1,7 @@
 #include "joedb/io/Interpreter.h"
 #include "joedb/io/Interpreter_Dump_Writable.h"
 #include "joedb/io/SQL_Dump_Writable.h"
+#include "joedb/io/Raw_Dump_Writable.h"
 #include "joedb/interpreter/Database.h"
 #include "joedb/Readable_Multiplexer.h"
 #include "gtest/gtest.h"
@@ -76,6 +77,27 @@ TEST_F(Interpreter_Test, SQL_Dump_Writable)
  std::ofstream("interpreter_test.sql.tmp") << dump_string.str();
 
  std::ifstream reference_file("interpreter_test.sql");
+ ASSERT_TRUE(reference_file.good());
+ std::ostringstream reference_string;
+ reference_string << reference_file.rdbuf();
+
+ EXPECT_EQ(reference_string.str(), dump_string.str());
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST_F(Interpreter_Test, Raw_Dump_Writable)
+{
+ std::ostringstream dump_string;
+ joedb::Raw_Dump_Writable writable(dump_string);
+ db.add_writable(writable);
+
+ std::ifstream in_file("interpreter_test.joedbi");
+ ASSERT_TRUE(in_file.good());
+ std::ostringstream out_string;
+ interpreter.main_loop(in_file, out_string);
+ std::ofstream("interpreter_test.raw.tmp") << dump_string.str();
+
+ std::ifstream reference_file("interpreter_test.raw");
  ASSERT_TRUE(reference_file.good());
  std::ostringstream reference_string;
  reference_string << reference_file.rdbuf();
