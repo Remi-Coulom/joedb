982f57d9d0ba3a5bec02ce689dd09ee245516f3c
Rémi Coulom <remi.coulom@gmail.com>
2021-07-10 14:14:13 +0200

    code formatting

--- src/joedb/Dummy_Writable.h
+++ src/joedb/Dummy_Writable.h
@@ -10,17 +10,39 @@ namespace joedb
   public:
    void create_table(const std::string &name) override {}
    void drop_table(Table_Id table_id) override {}
-   void rename_table(Table_Id table_id,
-                     const std::string &name) override {}
+   void rename_table
+   (
+    Table_Id table_id,
+    const std::string &name
+   ) override
+   {
+   }
 
-   void add_field(Table_Id table_id,
-                  const std::string &name,
-                  Type type) override {}
-   void drop_field(Table_Id table_id,
-                   Field_Id field_id) override {}
-   void rename_field(Table_Id table_id,
-                     Field_Id field_id,
-                     const std::string &name) override {}
+   void add_field
+   (
+    Table_Id table_id,
+    const std::string &name,
+    Type type
+   ) override
+   {
+   }
+
+   void drop_field
+   (
+    Table_Id table_id,
+    Field_Id field_id
+   ) override
+   {
+   }
+
+   void rename_field
+   (
+    Table_Id table_id,
+    Field_Id field_id,
+    const std::string &name
+   ) override
+   {
+   }
 
    void custom(const std::string &name) override {}
 
@@ -29,21 +51,36 @@ namespace joedb
    void valid_data() override {}
 
    void insert_into(Table_Id table_id, Record_Id record_id) override {}
-   void insert_vector(Table_Id table_id,
-                      Record_Id record_id,
-                      Record_Id size) override {}
+   void insert_vector
+   (
+    Table_Id table_id,
+    Record_Id record_id,
+    Record_Id size
+   ) override
+   {
+   }
    void delete_from(Table_Id table_id, Record_Id record_id) override {}
 
    #define TYPE_MACRO(type, return_type, type_id, R, W)\
-   void update_##type_id(Table_Id table_id,\
-                         Record_Id record_id,\
-                         Field_Id field_id,\
-                         return_type value) override {}\
-   void update_vector_##type_id(Table_Id table_id,\
-                                Record_Id record_id,\
-                                Field_Id field_id,\
-                                Record_Id size,\
-                                const type *value) override {};
+   void update_##type_id\
+   (\
+    Table_Id table_id,\
+    Record_Id record_id,\
+    Field_Id field_id,\
+    return_type value\
+   ) override\
+   {\
+   }\
+   void update_vector_##type_id\
+   (\
+    Table_Id table_id,\
+    Record_Id record_id,\
+    Field_Id field_id,\
+    Record_Id size,\
+    const type *value\
+   ) override\
+   {\
+   };
    #include "joedb/TYPE_MACRO.h"
  };
 }
--- src/joedb/Exception.h
+++ src/joedb/Exception.h
@@ -5,24 +5,47 @@
 
 namespace joedb
 {
+ ////////////////////////////////////////////////////////////////////////////
  class Runtime_Error: public std::runtime_error
+ ////////////////////////////////////////////////////////////////////////////
  {
   public:
-   explicit Runtime_Error(const char *what_arg): std::runtime_error(what_arg) {}
-   explicit Runtime_Error(const std::string &what_arg): std::runtime_error(what_arg) {}
+   explicit Runtime_Error(const char *what_arg):
+    std::runtime_error(what_arg)
+   {
+   }
+
+   explicit Runtime_Error(const std::string &what_arg):
+    std::runtime_error(what_arg)
+   {
+   }
  };
 
+ ////////////////////////////////////////////////////////////////////////////
  class Assertion_Failure: public Runtime_Error
+ ////////////////////////////////////////////////////////////////////////////
  {
   public:
-   explicit Assertion_Failure(const char *what_arg): Runtime_Error(what_arg) {}
+   explicit Assertion_Failure(const char *what_arg):
+    Runtime_Error(what_arg)
+   {
+   }
  };
 
+ ////////////////////////////////////////////////////////////////////////////
  class Exception: public Runtime_Error
+ ////////////////////////////////////////////////////////////////////////////
  {
   public:
-   explicit Exception(const char *what_arg): Runtime_Error(what_arg) {}
-   explicit Exception(const std::string &what_arg): Runtime_Error(what_arg) {}
+   explicit Exception(const char *what_arg):
+    Runtime_Error(what_arg)
+   {
+   }
+
+   explicit Exception(const std::string &what_arg):
+    Runtime_Error(what_arg)
+   {
+   }
  };
 }
 
