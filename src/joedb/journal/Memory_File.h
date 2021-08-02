95df139327fe481447db44e2e3d70c69b9675415
Rémi Coulom <remi.coulom@gmail.com>
2021-08-02 13:34:22 +0200

    make Visual C++ happy to run tests in debug mode.

--- src/joedb/journal/Memory_File.h
+++ src/joedb/journal/Memory_File.h
@@ -28,7 +28,12 @@ namespace joedb
    {
     const size_t max_size = data.size() - current;
     const size_t n = std::min(size, max_size);
-    std::copy_n(&data[current], n, buffer);
+#ifdef _DEBUG
+    if (n > 0) // This is needed for debug mode with Visual C++
+#endif
+    {
+     std::copy_n(&data[current], n, buffer);
+    }
     current += n;
     return n;
    }
