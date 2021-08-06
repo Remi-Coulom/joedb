47a671ae0b88106bb32a34ebfaa6652efe1b6904
Rémi Coulom <remi.coulom@gmail.com>
2021-08-06 21:42:26 +0200

    add missing try-catch in Client's lock_pull and push_unlock.

--- src/joedb/concurrency/Client.h
+++ src/joedb/concurrency/Client.h
@@ -19,28 +19,54 @@ namespace joedb
 
    int64_t server_position;
 
+   //////////////////////////////////////////////////////////////////////////
    void check_position()
+   //////////////////////////////////////////////////////////////////////////
    {
     if (journal.get_position() > server_position)
      throw Exception("Can't pull: failed transaction wrote to local db.");
    }
 
+   //////////////////////////////////////////////////////////////////////////
    void lock_pull()
+   //////////////////////////////////////////////////////////////////////////
    {
     check_position();
     server_position = connection.lock_pull(journal);
-    journal.play_until_checkpoint(writable);
+
+    try
+    {
+     journal.play_until_checkpoint(writable);
+    }
+    catch(...)
+    {
+     connection.unlock();
+     throw;
+    }
    }
 
+   //////////////////////////////////////////////////////////////////////////
    void push_unlock()
+   //////////////////////////////////////////////////////////////////////////
    {
-    journal.checkpoint(0);
+    try
+    {
+     journal.checkpoint(0);
+    }
+    catch(...)
+    {
+     connection.unlock();
+     throw;
+    }
+
     connection.push_unlock(journal, server_position);
     server_position = journal.get_checkpoint_position();
    }
 
   public:
+   //////////////////////////////////////////////////////////////////////////
    Client
+   //////////////////////////////////////////////////////////////////////////
    (
     Connection &connection,
     Writable_Journal &journal,
@@ -54,7 +80,9 @@ namespace joedb
     server_position = journal.get_position();
    }
 
+   //////////////////////////////////////////////////////////////////////////
    int64_t pull()
+   //////////////////////////////////////////////////////////////////////////
    {
     check_position();
     server_position = connection.pull(journal);
@@ -62,7 +90,9 @@ namespace joedb
     return server_position;
    }
 
+   //////////////////////////////////////////////////////////////////////////
    void write_transaction(std::function<void()> transaction)
+   //////////////////////////////////////////////////////////////////////////
    {
     lock_pull();
 
