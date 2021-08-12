5ce6305b0b0453bf81e7eb4a7fae40f7d5edd5fd
Rémi coulom <remi.coulom@gmail.com>
2021-08-12 23:54:27 +0200

    formatting

--- test/Server_Test.cpp
+++ test/Server_Test.cpp
@@ -15,18 +15,19 @@ TEST(Server, basic)
  joedb::Writable_Journal server_journal(server_file);
  net::io_context io_context;
  joedb::Server server(server_journal, io_context, 0, 0);
+ std::thread server_thread([&io_context](){io_context.run();});
 
- std::thread thread([&io_context](){io_context.run();});
+ std::ostringstream port_stream;
+ port_stream << server.get_port();
+ std::string port_string = port_stream.str();
+ const char * const port = port_string.c_str();
 
- std::ostringstream port_string;
- port_string << server.get_port();
-
- joedb::Network_Channel channel_1("localhost", port_string.str().c_str());
+ joedb::Network_Channel channel_1("localhost", port);
  joedb::Server_Connection connection_1(channel_1);
  joedb::Memory_File client_file_1;
  joedb::Interpreted_Client client_1(connection_1, client_file_1);
 
- joedb::Network_Channel channel_2("localhost", port_string.str().c_str());
+ joedb::Network_Channel channel_2("localhost", port);
  joedb::Server_Connection connection_2(channel_2);
  joedb::Memory_File client_file_2;
  joedb::Interpreted_Client client_2(connection_2, client_file_2);
@@ -46,5 +47,5 @@ TEST(Server, basic)
  EXPECT_EQ(client_2.get_database().get_tables().size(), 1ULL);
 
  server.interrupt();
- thread.join();
+ server_thread.join();
 }
