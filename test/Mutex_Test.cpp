e69353f508259623e68170fd57b5c5720f4185d2
Rémi Coulom <remi.coulom@gmail.com>
2021-08-14 17:58:08 +0200

    better Mutex test

--- test/Mutex_Test.cpp
+++ test/Mutex_Test.cpp
@@ -12,7 +12,19 @@ class Throwing_Mutex: public joedb::Mutex
 };
 
 /////////////////////////////////////////////////////////////////////////////
-TEST(Mutex, catcher)
+class Dummy_Mutex: public joedb::Mutex
+/////////////////////////////////////////////////////////////////////////////
+{
+ private:
+  void lock() override {locked = true;}
+  void unlock() override {locked = false;}
+
+ public:
+  bool locked = false;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(Mutex, unlock_throws)
 /////////////////////////////////////////////////////////////////////////////
 {
  Throwing_Mutex mutex;
@@ -26,4 +38,26 @@ TEST(Mutex, catcher)
  {
   EXPECT_STREQ(e.what(), "youyou");
  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+TEST(Mutex, transaction_throws)
+/////////////////////////////////////////////////////////////////////////////
+{
+ Dummy_Mutex mutex;
+
+ try
+ {
+  mutex.run_while_locked([]()
+  {
+   throw std::runtime_error("yaya");
+  });
+  FAIL() << "Did not catch the exception";
+ }
+ catch (const std::runtime_error &e)
+ {
+  EXPECT_STREQ(e.what(), "yaya");
+ }
+
+ EXPECT_FALSE(mutex.locked);
 }
