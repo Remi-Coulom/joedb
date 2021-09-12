c259e9d19d6f34eab975ef720885306d4000db5b
Rémi Coulom <remi.coulom@gmail.com>
2021-09-12 10:43:13 +0200

    Write messages to attempt to debug github CI

--- compcmake/generate.sh
+++ compcmake/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo Generating cmake build directories...
 set -e
 
 ninja_path=`which ninja`
@@ -10,6 +11,12 @@ else
  build_system_prefix=""
  echo Warning: ninja not available!
 fi
+
+echo
+echo =======================================================================
+echo ninja_path=$ninja_path
+echo build_system_prefix=$build_system_prefix
+echo build_system=$build_system
 
 function generate {
  echo
@@ -23,6 +30,12 @@ function generate {
 
 gpp_path=`which g++`
 gcc_path=`which gcc`
+
+echo
+echo =======================================================================
+echo gpp_path=$gpp_path
+echo gcc_path=$gcc_path
+
 if [ "$gcc_path" != "" ]; then
  compiler="-DCMAKE_CXX_COMPILER=$gpp_path -DCMAKE_C_COMPILER=$gcc_path"
  generate "$build_system_prefix"release cmake $build_system -DCMAKE_BUILD_TYPE=Release $compiler
@@ -32,6 +45,12 @@ fi
 
 clangpp_path=`which clang++`
 clang_path=`which clang`
+
+echo
+echo =======================================================================
+echo clangpp_path=$clangpp_path
+echo clang_path=$clang_path
+
 if [ "$clang_path" != "" ]; then
  generate "$build_system_prefix"clang cmake $build_system -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="$clangpp_path" -DCMAKE_C_COMPILER="$clang_path"
 fi
