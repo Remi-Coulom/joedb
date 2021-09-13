3a1d8c1fa1f400e35249dde5c07fd6c5f6a61dcb
Rémi Coulom <remi.coulom@gmail.com>
2021-09-14 01:04:33 +0200

    Test big-endian with cross-compilation, like ARM.

--- test/bigendian/start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-qemu-system-ppc -bios ./openbios-ppc -hda debian-wheezy-powerpc.qcow2 -L bios -m 2047M -g 1024x768x8 -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::2222-:22
