327b60e6d722a77e5b9544a1f6f4f13b4bce36fa
Rémi Coulom <remi.coulom@gmail.com>
2021-09-13 21:25:29 +0200

    optional client

--- src/joedb/io/joedb_embedded_client.cpp
+++ src/joedb/io/joedb_embedded_client.cpp
@@ -9,15 +9,15 @@ namespace joedb
  static int main(int argc, char **argv)
  /////////////////////////////////////////////////////////////////////////////
  {
-  if (argc != 3)
+  if (argc < 2)
   {
-   std::cerr << "usage: " << argv[0] << " <server.joedb> <client.joedb>\n";
+   std::cerr << "usage: " << argv[0] << " <server.joedb> [<client.joedb>]\n";
    return 1;
   }
   else
   {
    const char * const server_file_name = argv[1];
-   const char * const client_file_name = argv[2];
+   const char * const client_file_name = argc > 2 ? argv[2] : nullptr;
 
    File server_file(server_file_name, Open_Mode::write_existing_or_create_new);
    Embedded_Connection connection(server_file);
