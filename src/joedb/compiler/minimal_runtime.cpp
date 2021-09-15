22b043d4322ed2d309c36dc302845aa2e32a176d
Rémi Coulom <remi.coulom@gmail.com>
2021-09-15 23:38:09 +0200

    minimal_runtime

--- src/joedb/compiler/minimal_runtime.cpp
+++ src/joedb/compiler/minimal_runtime.cpp
@@ -1,9 +1,10 @@
 // minimal set of files to be linked with a compiled database
-#include "../Writable.cpp"
 #include "../Destructor_Logger.cpp"
+#include "../Writable.cpp"
 #include "../compiler/minimal_runtime_io.cpp"
 #include "../journal/File.cpp"
 #include "../journal/Generic_File.cpp"
-#include "../journal/Writable_Journal.cpp"
+#include "../journal/Memory_File.cpp"
 #include "../journal/Readonly_Journal.cpp"
+#include "../journal/Writable_Journal.cpp"
 #include "../journal/SHA_256.cpp"
