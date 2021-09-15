7ba8dbb09fd4b573fafc3bef3856e2ad2281f537
Rémi Coulom <remi.coulom@gmail.com>
2021-09-15 22:39:33 +0200

    no more inline functions for Network_Channel

--- src/joedb/concurrency/Network_Channel.h
+++ src/joedb/concurrency/Network_Channel.h
@@ -17,19 +17,8 @@ namespace joedb
 
    std::mutex &get_mutex() override {return mutex;}
 
-   //////////////////////////////////////////////////////////////////////////
-   size_t write_some(const char *data, size_t size) override
-   //////////////////////////////////////////////////////////////////////////
-   {
-    return socket.write_some(net::buffer(data, size));
-   }
-
-   //////////////////////////////////////////////////////////////////////////
-   size_t read_some(char *data, size_t size) override
-   //////////////////////////////////////////////////////////////////////////
-   {
-    return socket.read_some(net::buffer(data, size));
-   }
+   size_t write_some(const char *data, size_t size) override;
+   size_t read_some(char *data, size_t size) override;
 
   public:
    Network_Channel(const char *host_name, const char *port_name);
