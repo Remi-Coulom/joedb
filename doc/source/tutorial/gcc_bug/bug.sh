b7d1210d00aef11e2f0ffae74aa3bdf05cfd4181
Rémi Coulom <remi.coulom@gmail.com>
2022-04-30 17:03:46 +0200

    more bug experiments in cygwin

--- doc/source/tutorial/gcc_bug/bug.sh
+++ doc/source/tutorial/gcc_bug/bug.sh
@@ -8,41 +8,39 @@
 #    * 10.3.0 -> ../../../../src/joedb/journal/Readonly_Journal.h:87:24: runtime error: execution reached an unreachable program point
 #    * 11.1.0 -> ../../../../src/joedb/journal/Readonly_Journal.h:87:24: runtime error: execution reached an unreachable program point
 #  - Cgywin (32 bits)
-#    * 11.2.0 ->
+#    * 11.2.0 -> infinite loop (but no ubsan)
 #
-# It works fine with clang or msvc.
-#
-# Compiling without -flto does not produce any warning, but -flto has one:
-# ../../../../src/joedb/journal/Readonly_Journal.cpp:396:22: warning:  may be used uninitialized in this function [-Wmaybe-uninitialized]
-#  396 |   return Type(type_id);
-# It is strange, because it seems that the unused variable name is empty.
+# -O1 instead of -O3 works well
 #
 # In bug.cpp, not catching exceptions in main makes the bug disappear (but not the strange warning).
 #
-# With OPTIONS="", the program either runs an infinite loop, or crashes.
+# Without -fsanitize=undefined, the program either runs an infinite loop, or crashes.
 #
-# Symptoms are a bit similar to:
-# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=87525
-# infinite loop generated for fread() if enabling -flto and -D_FORTIFY_SOURCE=2
-# But I am not sure it is the same problem
+# Compiling without -flto does not produce any warning, but -flto has one with g++ version < 11:
+# ../../../../src/joedb/journal/Readonly_Journal.cpp:396:22: warning:  may be used uninitialized in this function [-Wmaybe-uninitialized]
+#  396 |   return Type(type_id);
+# It is strange, because it seems that the unused variable name is empty.
+# This strange warning disappeared with 11.1.0, so it may be an unrelated bug that has been fixed already.
 #
 
 set -e
 g++ --version
 set -o xtrace
 
-OPTIONS="-Wall -Wextra -Wno-unused-parameter -fno-strict-aliasing -fwrapv -fno-aggressive-loop-optimizations -fsanitize=undefined"
-#OPTIONS=""
+OPTIONS="-O3 -Wall -Wextra -Wno-unused-parameter -fno-strict-aliasing -fwrapv -fno-aggressive-loop-optimizations"
+if [ "$OSTYPE" != "cygwin" ]; then
+ OPTIONS="${OPTIONS} -fsanitize=undefined"
+fi
 
 # Generate .ii file for bug report
 g++ -save-temps -DNDEBUG -I ../../../../src ${OPTIONS} -o bug bug.cpp
 
 # Compiling without LTO works OK
-g++ -O3 ${OPTIONS} -o bug bug.ii
+g++ ${OPTIONS} -o bug bug.ii
 ./bug
 
 # LTO usually does not work well
-g++ -O3 -flto ${OPTIONS} -o bug bug.ii
+g++ ${OPTIONS} -flto -o bug bug.ii
 echo This is likely to crash or hang in an infinite loop...
 ./bug
 echo Yeah, no bug!
