a210c4a140051a17f10683d4767ee946194478ff
Rémi Coulom <remi.coulom@gmail.com>
2022-09-13 20:55:17 +0900

    compile again in Ubuntu 16.04

--- compcmake/compiler_flags.cmake
+++ compcmake/compiler_flags.cmake
@@ -3,8 +3,11 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 #############################################################################
  message("== gcc")
  set(CMAKE_CXX_FLAGS
-  "-Wall -Wextra -Wno-unused-parameter -Wduplicated-cond -Wshadow=local -pedantic -Wconversion -Wstack-usage=131072 -Wunused-macros -Wc++14-compat -Wc++17-compat -Wcast-qual -Wcast-align -Wparentheses -Wlogical-op -Wmissing-declarations -Wredundant-decls"
+  "-Wall -Wextra -Wno-unused-parameter -pedantic -Wconversion -Wstack-usage=131072 -Wunused-macros -Wc++14-compat -Wcast-qual -Wcast-align -Wparentheses -Wlogical-op -Wmissing-declarations -Wredundant-decls"
  )
+ if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.4)
+  set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-Wduplicated-cond -Wshadow=local -Wc++17-compat")
+ endif()
  set(CMAKE_CXX_FLAGS_COVERAGE
   "-g -O0 -fno-inline -fno-default-inline -fno-inline-small-functions --coverage"
  )
