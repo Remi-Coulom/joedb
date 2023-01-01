abbc97e2390e19cac68050fe68838ea32f396139
Rémi Coulom <remi.coulom@gmail.com>
2023-01-01 18:32:03 +0100

    cross-compiled arm version in github workflows

--- compcmake/arm.toolchain.cmake
+++ compcmake/arm.toolchain.cmake
@@ -1,3 +1,6 @@
+# https://github.com/WojciechMigda/how-to-qemu-arm-gdb-gtest
+# sudo apt install gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf qemu-system-arm qemu-user gdb-multiarch
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
