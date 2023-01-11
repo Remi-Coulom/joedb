0bdb6dac94db18a5b29390b26b18a831d7fa8980
Rémi Coulom <remi.coulom@gmail.com>
2023-01-11 15:23:25 +0100

    pthread compiler option

--- compcmake/compiler_flags.cmake
+++ compcmake/compiler_flags.cmake
@@ -3,7 +3,7 @@ if(CMAKE_COMPILER_IS_GNUCXX)
 #############################################################################
  message("== gcc")
  set(CMAKE_CXX_FLAGS
-  "${CMAKE_CXX_FLAGS} -Wall -Wextra -Wno-unused-parameter -pedantic -Wconversion -Wstack-usage=131072 -Wunused-macros -Wc++14-compat -Wcast-qual -Wcast-align -Wparentheses -Wlogical-op -Wmissing-declarations -Wredundant-decls"
+  "${CMAKE_CXX_FLAGS} -pthread -Wall -Wextra -Wno-unused-parameter -pedantic -Wconversion -Wstack-usage=131072 -Wunused-macros -Wc++14-compat -Wcast-qual -Wcast-align -Wparentheses -Wlogical-op -Wmissing-declarations -Wredundant-decls"
  )
  if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.4)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wduplicated-cond -Wshadow=local -Wc++17-compat")
