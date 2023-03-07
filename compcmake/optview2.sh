d0e5bbe44d32e048306ecc2958f53e915f5c796e
Rémi Coulom <remi.coulom@gmail.com>
2023-03-07 17:28:31 +0100

    optview2

--- compcmake/optview2.sh
+++ compcmake/optview2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #https://github.com/OfekShilon/optview2
-~/repos/optview2/opt-viewer.py -j4 --output-dir clang_release/optview2 --source-dir .. ./clang_release/CMakeFiles
-echo xdg-open clang_release/optview2/index.html
+output_dir=optview2
+~/repos/optview2/opt-viewer.py -j4 --output-dir ${output_dir} --source-dir .. .
+echo xdg-open ${output_dir}/index.html
