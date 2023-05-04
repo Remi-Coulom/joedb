61d9c7be93f6907593ce5ca9898871b6510538ff
Rémi Coulom <remi.coulom@gmail.com>
2023-05-05 00:48:46 +0200

    use memory file with joedbi so that blobs work without file name

--- src/joedb/io/joedbi.cpp
+++ src/joedb/io/joedbi.cpp
@@ -15,24 +15,15 @@ namespace joedb
  static int main(int argc, char **argv)
  /////////////////////////////////////////////////////////////////////////////
  {
-  Database db;
+  std::unique_ptr<Generic_File> file;
 
-  if (argc <= 1)
-  {
-   Interpreter interpreter(db, db);
-   interpreter.main_loop(std::cin, std::cout);
-  }
-  else
+  if (argc > 1)
   {
    const char * const file_name = argv[1];
-
-   std::unique_ptr<Generic_File> file;
-   std::unique_ptr<Writable_Journal> writable_journal;
 
    try
    {
     file.reset(new File(file_name, Open_Mode::write_existing_or_create_new));
-    writable_journal.reset(new Writable_Journal(*file));
    }
    catch (const Exception &e)
    {
@@ -40,23 +31,29 @@ namespace joedb
     std::cout << "Opening file read-only.\n";
     file.reset(new File(file_name, Open_Mode::read_existing));
    }
+  }
+  else
+   file.reset(new Memory_File());
 
-   db.set_blob_storage(file.get());
+  std::unique_ptr<Writable_Journal> writable_journal;
+  writable_journal.reset(new Writable_Journal(*file));
 
-   if (file->get_mode() == Open_Mode::read_existing)
-   {
-    Readonly_Journal journal(*file);
-    journal.replay_log(db);
-    Readonly_Interpreter interpreter(db);
-    interpreter.main_loop(std::cin, std::cout);
-   }
-   else
-   {
-    writable_journal->replay_log(db);
-    Multiplexer multiplexer{db, *writable_journal};
-    Interpreter interpreter(db, multiplexer);
-    interpreter.main_loop(std::cin, std::cout);
-   }
+  Database db;
+  db.set_blob_storage(file.get());
+
+  if (file->get_mode() == Open_Mode::read_existing)
+  {
+   Readonly_Journal journal(*file);
+   journal.replay_log(db);
+   Readonly_Interpreter interpreter(db);
+   interpreter.main_loop(std::cin, std::cout);
+  }
+  else
+  {
+   writable_journal->replay_log(db);
+   Multiplexer multiplexer{db, *writable_journal};
+   Interpreter interpreter(db, multiplexer);
+   interpreter.main_loop(std::cin, std::cout);
   }
 
   return 0;
