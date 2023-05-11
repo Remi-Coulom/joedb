97aa8b68f36a3c7f3b9a41954be73379ad1ea918
Rémi Coulom <remi.coulom@gmail.com>
2023-05-11 16:11:14 +0200

    --fast option for joedb_sha256sum

--- src/joedb/io/joedb_sha256sum.cpp
+++ src/joedb/io/joedb_sha256sum.cpp
@@ -10,15 +10,27 @@ int main(int argc, char **argv)
  std::cout << std::hex;
  std::cout << std::setfill('0');
 
- for (int arg_index = 1; arg_index < argc; arg_index++)
+ bool fast = false;
+ int arg_start = 1;
+
+ if (argc > 1 && argv[1] == std::string("--fast"))
+ {
+  fast = true;
+  arg_start++;
+ }
+
+ for (int arg_index = arg_start; arg_index < argc; arg_index++)
  {
   const char * const file_name = argv[arg_index];
 
   try
   {
    joedb::File file(file_name, joedb::Open_Mode::read_existing);
-//   joedb::SHA_256::Hash hash = file.get_hash();
-   joedb::SHA_256::Hash hash = file.get_fast_hash(41, file.get_size());
+
+   const joedb::SHA_256::Hash hash = fast ?
+    file.get_fast_hash(0, file.get_size()) :
+    file.get_hash();
+
    for (uint32_t i = 0; i < 8; i++)
     std::cout << std::setw(8) << hash[i];
    std::cout << "  " << file_name << '\n';
