b1c1253de797fa887aeaaab6e69cee2e6568ad7c
Rémi Coulom <remi.coulom@gmail.com>
2023-08-17 13:55:44 +0200

    full_test.sh can take configurations as parameters

--- test/full_test.sh
+++ test/full_test.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 set -e
 
-cd ../compcmake
-./generate.sh
-configurations=`find . -maxdepth 1 -mindepth 1 -type d -printf ' %f'`
-cd -
+if [ "$1" == "" ]; then
+ cd ../compcmake
+ ./generate.sh
+ configurations=`find . -maxdepth 1 -mindepth 1 -type d -printf ' %f'`
+ cd -
+else
+ if [ "$1" == "usual" ]; then
+  configurations="gcc_asan gcc_debug gcc_portable clang_release arm_release clang_release"
+ else
+  configurations=$*
+ fi
 
-echo configurations:${configurations}
+ echo configurations: ${configurations}
+ sleep 2
+fi
 
 export WINEPATH="/usr/x86_64-w64-mingw32/lib\;/usr/lib/gcc/x86_64-w64-mingw32/9.3-posix"
 
@@ -14,6 +23,9 @@ for configuration in ${configurations}; do
  echo =======================================================================
  echo ${configuration}
  echo =======================================================================
+ cd ../compcmake
+ ./generate.sh $configuration
+ cd -
  build_dir=../compcmake/${configuration}
  cd $build_dir
  ctest -V
