2673f2b264271b0fa41467053f6cf35cc594f0d1
Rémi Coulom <remi.coulom@gmail.com>
2023-12-18 11:13:18 +0100

    ndk-cmake

---
+++ compcmake/ndk-cmake.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NDK=~/android-ndk-r26b
+ABI=arm64-v8a
+MINSDKVERSION=29
+cmake \
+    -DCMAKE_TOOLCHAIN_FILE=$NDK/build/cmake/android.toolchain.cmake \
+    -DANDROID_ABI=$ABI \
+    -DANDROID_PLATFORM=android-$MINSDKVERSION \
+    -G Ninja $*
