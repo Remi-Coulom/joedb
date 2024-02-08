077131dc296397c9dcd34d3c2191ce2a786f6df2
Rémi Coulom <remi.coulom@gmail.com>
2024-02-08 19:10:01 +0100

    properly set clang-tidy when cross-compiling with the ndk

--- compcmake/compiler_flags.cmake
+++ compcmake/compiler_flags.cmake
@@ -40,11 +40,11 @@ if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 #############################################################################
  message("== clang")
 
- find_program(HAS_CLANG_TIDY clang-tidy)
+ find_program(CLANG_TIDY clang-tidy)
+ message("-- CLANG_TIDY = ${CLANG_TIDY}")
+
  if (HAS_CLANG_TIDY)
-  set(CMAKE_CXX_CLANG_TIDY clang-tidy --header-filter=* -checks=-*,readability-*,-readability-braces-around-statements,-readability-magic-numbers,-readability-implicit-bool-conversion,-readability-else-after-return,-readability-uppercase-literal-suffix,-readability-static-accessed-through-instance,-readability-identifier-length,-readability-function-cognitive-complexity,-readability-container-data-pointer,-readability-use-anyofallof,bugprone-*,-bugprone-macro-parentheses,-bugprone-exception-escape,-bugprone-branch-clone,-bugprone-string-constructor,-bugprone-suspicious-include,-bugprone-easily-swappable-parameters,-bugprone-implicit-widening-of-multiplication-result,concurrency-*,modernize-*,-modernize-use-trailing-return-type,-modernize-use-auto,-modernize-raw-string-literal,-modernize-avoid-c-arrays,-modernize-deprecated-headers,-modernize-loop-convert,-modernize-return-braced-init-list,-modernize-use-default-member-init,-modernize-use-using,-modernize-concat-nested-namespaces)
- else()
-  message("-- no clang-tidy")
+  set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY} --header-filter=* -checks=-*,readability-*,-readability-braces-around-statements,-readability-magic-numbers,-readability-implicit-bool-conversion,-readability-else-after-return,-readability-uppercase-literal-suffix,-readability-static-accessed-through-instance,-readability-identifier-length,-readability-function-cognitive-complexity,-readability-container-data-pointer,-readability-use-anyofallof,bugprone-*,-bugprone-macro-parentheses,-bugprone-exception-escape,-bugprone-branch-clone,-bugprone-string-constructor,-bugprone-suspicious-include,-bugprone-easily-swappable-parameters,-bugprone-implicit-widening-of-multiplication-result,concurrency-*,modernize-*,-modernize-use-trailing-return-type,-modernize-use-auto,-modernize-raw-string-literal,-modernize-avoid-c-arrays,-modernize-deprecated-headers,-modernize-loop-convert,-modernize-return-braced-init-list,-modernize-use-default-member-init,-modernize-use-using,-modernize-concat-nested-namespaces)
  endif()
 
  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread -Wall -Wextra -Wpedantic -Wno-unused-parameter -Wno-gnu-zero-variadic-macro-arguments -Wunused-macros -Wcast-qual -Wcast-align -Wparentheses -Wmissing-declarations")
