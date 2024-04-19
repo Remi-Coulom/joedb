7898cb9561b938eda616bfa9f55a894462f1e3de
Rémi Coulom <remi.coulom@gmail.com>
2024-04-19 21:55:59 +0200

    no * in test script

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,11 @@
 build_dir=../compcmake/gcc_debug
 cmake --build $build_dir --target joedb_test || exit 1
 rm -vf *.tmp
-$build_dir/joedb_test --gtest_filter="$1*"
+
+if [ "$1" == "" ]; then
+ filter='*'
+else
+ filter="$1"
+fi
+
+$build_dir/joedb_test --gtest_filter="$filter"
