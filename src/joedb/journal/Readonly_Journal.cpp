2fe7c29bb5b3b7b0f014611f0d9db5e13aabc4b9
Rémi Coulom <remi.coulom@gmail.com>
2024-04-29 11:40:50 +0200

    Revert "remove locked_tail flag: it is a performance optimization that is not worth the added complexity."
    This reverts commit 6b876aa51d7e1e41525a6d67da7eb5cbc24bed4e.

--- src/joedb/journal/Readonly_Journal.cpp
+++ src/joedb/journal/Readonly_Journal.cpp
@@ -156,9 +156,13 @@ void joedb::Readonly_Journal::pull()
  std::array<int64_t, 4> pos;
 
  {
-  file.shared_lock_head();
+  if (!file.tail_is_locked())
+   file.shared_lock_head();
+
   file.raw_pread((char *)&pos, sizeof(pos), checkpoint_offset);
-  file.unlock_head();
+
+  if (!file.tail_is_locked())
+   file.unlock_head();
  }
 
  read_checkpoint(pos);
