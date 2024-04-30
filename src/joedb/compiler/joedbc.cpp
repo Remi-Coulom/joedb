ac6350651b66daeb5871a461c1dbd0e7a61abc3c
Rémi Coulom <remi.coulom@gmail.com>
2024-04-30 21:25:51 +0200

    get_database() of Client can be const

--- src/joedb/compiler/joedbc.cpp
+++ src/joedb/compiler/joedbc.cpp
@@ -559,9 +559,8 @@ static void generate_h(std::ostream &out, const Compiler_Options &options)
     schema_checkpoint = db.schema_journal.get_checkpoint_position();
    }
 
-   const Database &get_database()
+   const Database &get_database() const
    {
-    update();
     return db;
    }
 
