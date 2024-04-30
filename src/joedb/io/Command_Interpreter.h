c51ca3c2ae1235029d85f542bf188c01c3d36718
Rémi Coulom <remi.coulom@gmail.com>
2024-04-30 19:04:11 +0200

    move Command_Interpreter functions from .h to .cpp

--- src/joedb/io/Command_Interpreter.h
+++ src/joedb/io/Command_Interpreter.h
@@ -41,50 +41,14 @@ namespace joedb
    ) override;
 
   public:
-   Command_Interpreter()
-   {
-    add_processor(*static_cast<Command_Processor *>(this));
-   }
-
-   void add_processor(Command_Processor &processor)
-   {
-    processors.emplace_back(processor);
-   }
-
-   void set_parent(const Command_Interpreter *new_parent)
-   {
-    parent = new_parent;
-
-    if (parent)
-    {
-     echo = parent->echo;
-     rethrow = parent->rethrow;
-     prompt = parent->prompt;
-    }
-   }
-
-   virtual void write_prompt(std::ostream &out) const
-   {
-    out << "joedbi";
-   }
-
-   void write_whole_prompt(std::ostream &out) const
-   {
-    if (parent)
-    {
-     parent->write_prompt(out);
-     out << '/';
-    }
-
-    write_prompt(out);
-    out << "> ";
-    out.flush();
-   }
-
+   Command_Interpreter();
+   void add_processor(Command_Processor &processor);
+   void set_parent(const Command_Interpreter *new_parent);
+   virtual void write_prompt(std::ostream &out) const;
+   void write_whole_prompt(std::ostream &out) const;
    void set_echo(bool new_echo) {echo = new_echo;}
    void set_rethrow(bool new_rethrow) {rethrow = new_rethrow;}
    void set_prompt(bool new_prompt) {prompt = new_prompt;}
-
    void main_loop(std::istream &in, std::ostream &out);
  };
 }
