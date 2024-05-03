3c4eb34a4efeeed5fc2a5bdff025d8b93196cfa4
Rémi Coulom <remi.coulom@gmail.com>
2024-05-03 15:11:34 +0200

    tests

--- compcmake/libjoedb.cmake
+++ compcmake/libjoedb.cmake
@@ -61,13 +61,13 @@ set(JOEDB_SOURCES
  ${JOEDB_SRC_DIR}/joedb/concurrency/Client.cpp
  ${JOEDB_SRC_DIR}/joedb/concurrency/Client_Data.cpp
  ${JOEDB_SRC_DIR}/joedb/concurrency/Connection.cpp
- ${JOEDB_SRC_DIR}/joedb/concurrency/IO_Context_Wrapper.cpp
  ${JOEDB_SRC_DIR}/joedb/concurrency/Server_Connection.cpp
  ${JOEDB_SRC_DIR}/joedb/compiler/nested_namespace.cpp
 )
 
 if (HAS_NETWORKING)
  set(JOEDB_SOURCES ${JOEDB_SOURCES}
+  ${JOEDB_SRC_DIR}/joedb/concurrency/IO_Context_Wrapper.cpp
   ${JOEDB_SRC_DIR}/joedb/concurrency/Server.cpp
   ${JOEDB_SRC_DIR}/joedb/concurrency/Network_Channel.cpp
  )
