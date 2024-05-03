672bae0e0733e249b45ed2d3e2c1cf9179b9f3f3
Rémi Coulom <remi.coulom@gmail.com>
2024-05-03 16:55:00 +0200

    this might fix github actions

--- compcmake/arm.toolchain.cmake
+++ compcmake/arm.toolchain.cmake
@@ -7,6 +7,11 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 
+set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
 set(CMAKE_CROSSCOMPILING TRUE)
 set(CMAKE_CROSSCOMPILING_EMULATOR
  "/usr/bin/qemu-arm;-L;/usr/arm-linux-gnueabihf"
