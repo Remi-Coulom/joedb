27b651a17a8c3f83ef98f6f8dace013ef46be5d1
Rémi Coulom <remi.coulom@gmail.com>
2024-07-11 01:02:48 +0200

    script for code size

---
+++ scripts/code_size.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+if [ -d db ]; then
+ cd db
+ for i in *.joedbi; do
+  b=$(basename $i .joedbi)
+  rm -vf $b*.{h,cpp}
+ done
+ cd -
+fi
+
+sources="$(find -name "*.h") $(find -name "*.c") $(find -name "*.hpp") $(find -name "*.cpp")"
+echo $sources
+echo "Number of lines:"
+cat $sources | wc -l
+echo "Size of gzipped source without comments:"
+cat $sources | gcc -fpreprocessed -w -dD -E - | gzip | wc -c
