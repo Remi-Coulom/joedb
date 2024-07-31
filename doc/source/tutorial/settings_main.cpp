952b73762a444112bd4f4306e6f18ceabddc65a0
Rémi Coulom <remi.coulom@gmail.com>
2024-07-31 20:48:17 +0200

    update settings tutorial to use a writable file

--- doc/source/tutorial/settings_main.cpp
+++ doc/source/tutorial/settings_main.cpp
@@ -1,14 +1,12 @@
-#include "settings_readonly.h"
+#include "settings.h"
 #include "joedb/journal/Interpreted_File.h"
 
 /////////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 /////////////////////////////////////////////////////////////////////////////
 {
- settings::Readonly_Database db
- (
-  joedb::Interpreted_File("../custom_settings.joedbi")
- );
+ joedb::Interpreted_File settings_file("../custom_settings.joedbi");
+ settings::Generic_File_Database db(settings_file);
 
  std::cout << "dark mode: ";
  std::cout << db.get_dark_mode(db.get_Settings_table().first());
