818a3ff97fbc11140c59e47bdceab269fcbd2b74
Rémi Coulom <remi.coulom@gmail.com>
2024-09-17 21:21:31 +0200

    fix arm toolchain

--- compcmake/arm.toolchain.cmake
+++ compcmake/arm.toolchain.cmake
@@ -6,7 +6,7 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
-set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -Wno-psabi) #https://stackoverflow.com/questions/48149323/what-does-the-gcc-warning-project-parameter-passing-for-x-changed-in-gcc-7-1-m
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-psabi") #https://stackoverflow.com/questions/48149323/what-does-the-gcc-warning-project-parameter-passing-for-x-changed-in-gcc-7-1-m
 
 set(CMAKE_FIND_ROOT_PATH /usr/arm-linux-gnueabihf)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
