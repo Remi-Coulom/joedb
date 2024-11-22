adc4d62d556beabe3b571536f46ebe5a520802f9
Rémi Coulom <remi.coulom@gmail.com>
2024-11-22 15:28:23 +0100

    move all lexers to new syntax directory

---
+++ syntax/pygments/joedbc_lexer.py
@@ -0,0 +1,40 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+class JoedbcLexer(RegexLexer):
+    name = 'joedbc'
+    aliases = ['joedbc']
+    filenames = ['*.joedbc']
+
+    tokens = {
+        'root': [
+            (r'namespace\s+', Keyword, 'namespace'),
+            (r'create_unique_index\s+', Keyword, 'index_table_fields'),
+            (r'create_index\s+', Keyword, 'index_table_fields'),
+            (r'set_table_null_initialization\s+', Keyword, 'table_constant'),
+            (r'generate_c_wrapper', Keyword)
+        ],
+        'namespace': [
+            (r'[a-zA-Z_]\w*', Name.Namespace, 'namespace_continuation')
+        ],
+        'namespace_continuation': [
+            (r'::', Operator, 'namespace')
+        ],
+        'index_table_fields': [
+            (r'[a-zA-Z_]\w*\s+', Name.Variable, 'table_fields')
+        ],
+        'table_fields': [
+            (r'[a-zA-Z_]\w*\s+', Name.Class, 'fields')
+        ],
+        'fields': [
+            (r'[a-zA-Z_]\w*', Name.Variable, 'fields_continuation')
+        ],
+        'fields_continuation': [
+            (r',', Operator, 'fields')
+        ],
+        'table_constant' : [
+        ]
+    }
+
+def setup(app):
+    app.add_lexer('joedbc', JoedbcLexer)
---
+++ syntax/pygments/joedbi_lexer.py
@@ -0,0 +1,68 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+class JoedbiLexer(RegexLexer):
+    name = 'joedbi'
+    aliases = ['joedbi']
+    filenames = ['*.joedbi']
+
+    tokens = {
+        'root': [
+            (r'create_table\s+', Keyword, 'table'),
+            (r'add_field\s+', Keyword, 'table_field_type_init'),
+            (r'custom\s+', Keyword, 'custom'),
+            (r'comment\s+', Keyword, 'literal'),
+            (r'insert_into\s+', Keyword, 'table_literal'),
+            (r'update\s+', Keyword, 'table_integer_field_literal'),
+            (r'delete_from\s+', Keyword, 'table_integer'),
+            (r'timestamp\s+', Keyword, 'timestamp'),
+            (r'.+', Literal)
+        ],
+        'table': [
+            (r'[a-zA-Z_]\w*', Name.Class)
+        ],
+        'table_field_type_init': [
+            (r'[a-zA-Z_]\w*\s+', Name.Class, 'field_type_init')
+        ],
+        'field_type_init': [
+            (r'[a-zA-Z_]\w*\s+', Name.Variable, 'type_init')
+        ],
+        'type_init': [
+            (r'references\s+[a-zA-Z_]\w*', Keyword.Type, 'init'),
+            (r'[a-zA-Z_]\w*', Keyword.Type, 'init')
+        ],
+        'init': [
+            (r'\s*=\s*', Operator, 'literal')
+        ],
+        'custom': [
+            (r'[a-zA-Z_]\w*', Name.Function)
+        ],
+        'literal': [
+            (r'\s*\"([^\\\"]|\\.)*\"', Literal.String),
+            (r'\s*\d+', Number.Integer)
+        ],
+        'integer': [
+            (r"\d+", Number.Integer)
+        ],
+        'table_literal': [
+            (r'[a-zA-Z_]\w*\s+', Name.Class, 'literal')
+        ],
+        'table_integer_field_literal': [
+            (r'[a-zA-Z_]\w*\s+', Name.Class, 'integer_field_literal')
+        ],
+        'integer_field_literal': [
+            (r"\d+\s+", Number.Integer, 'field_literal')
+        ],
+        'field_literal': [
+            (r'[a-zA-Z_]\w*\s+', Name.Variable, 'literal')
+        ],
+        'table_integer': [
+            (r'[a-zA-Z_]\w*\s+', Name.Class, 'integer')
+        ],
+        'timestamp': [
+            (r'[a-zA-Z0-9\ \-\:]+', Literal.String)
+        ]
+    }
+
+def setup(app):
+    app.add_lexer('joedbi', JoedbiLexer)
