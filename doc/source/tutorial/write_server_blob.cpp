db43371f46dd3250c9f42933051e566837650e11
Rémi Coulom <remi.coulom@gmail.com>
2025-04-04 11:09:20 +0200

    write_server_blob.cpp example with joedb::Client_Lock

--- doc/source/tutorial/write_server_blob.cpp
+++ doc/source/tutorial/write_server_blob.cpp
@@ -29,12 +29,28 @@ static int write_server_blob(int argc, char **argv)
  // Creating the client: server file serves both as file and connection
  joedb::Journal_Client client(server_file, server_file);
 
- // Writing a new blob
- const joedb::Blob blob = client.write_blob_data(argv[2]);
+ // Write blobs with a client: keeps the server locked between writes
+ {
+  joedb::Client_Lock lock(client);
+  joedb::Writable_Journal &journal = lock.get_journal();
+  journal.seek_to_checkpoint();
 
- // Reading it back (no need of client, can be read from file directly)
- std::cout << "blob: " << server_file.read_blob_data(blob) << '\n';
- std::cout << "blob position: " << blob.get_position() << '\n';
+  for (int i = 3; --i >= 0;)
+  {
+   const joedb::Blob blob = journal.write_blob_data(argv[2]);
+   journal.default_checkpoint();
+   lock.push();
+   std::cout << "wrote blob with lock: " << blob.get_position() << '\n';
+   std::cout << "blob: " << server_file.read_blob_data(blob) << '\n';
+  }
+ }
+
+ // Writing a new blob: this uses a new transaction lambda for each write
+ {
+  const joedb::Blob blob = client.write_blob_data(argv[2]);
+  std::cout << "wrote blob with transaction: " << blob.get_position() << '\n';
+  std::cout << "blob: " << server_file.read_blob_data(blob) << '\n';
+ }
 
  return 0;
 }
