7d65c19053e6a1e494240d804632e8fcc17a6f27
Rémi Coulom <remi.coulom@gmail.com>
2025-04-20 19:25:52 +0200

    pull will always be done from a checkpoint, no need to flush.

--- src/joedb/concurrency/Server_Connection.cpp
+++ src/joedb/concurrency/Server_Connection.cpp
@@ -70,7 +70,6 @@ namespace joedb
    buffer.index = 0;
    lock.read(buffer.data, 8);
    const int64_t size = buffer.read<int64_t>();
-   client_journal->flush(); // ??? necessary ???
    const int64_t old_position = client_journal->get_position();
    Async_Writer writer = client_journal->get_async_tail_writer();
    download(writer, lock, size);
