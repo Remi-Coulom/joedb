85c8ed234c12fcccfcacbe1e5ec35ccc0470e14b
Rémi Coulom <remi.coulom@gmail.com>
2025-05-07 15:42:51 +0200

    fix Readonly_Database_Client

--- src/joedb/concurrency/Readonly_Database_Client.h
+++ src/joedb/concurrency/Readonly_Database_Client.h
@@ -6,31 +6,16 @@
 
 namespace joedb
 {
- namespace detail
+ /// @ingroup concurrency
+ class Readonly_Database_Client: public Readonly_Client
  {
-  class Readonly_Database_Client_Data
-  {
-   protected:
-    Readonly_Journal data_journal;
+  private:
     Database database;
 
-   public:
-    Readonly_Database_Client_Data(Buffered_File &file):
-     data_journal(file)
-    {
-    }
-  };
- };
-
- /// @ingroup concurrency
- class Readonly_Database_Client:
-  protected detail::Readonly_Database_Client_Data,
-  public Readonly_Client
- {
   protected:
    void read_journal() override
    {
-    data_journal.play_until_checkpoint(database);
+    Readonly_Journal::play_until_checkpoint(database);
    }
 
   public:
@@ -40,8 +25,7 @@ namespace joedb
     Connection &connection,
     Content_Check content_check = Content_Check::quick
    ):
-    Readonly_Database_Client_Data(file),
-    Readonly_Client(data_journal, connection, content_check)
+    Readonly_Client(file, connection, content_check)
    {
     read_journal();
    }
