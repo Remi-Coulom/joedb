5746f0714bb4e46d3f28f58dd24b14e47b37f02f
Rémi Coulom <remi.coulom@gmail.com>
2025-05-21 17:53:38 +0200

    formatting

--- test/ui_Test.cpp
+++ test/ui_Test.cpp
@@ -5,23 +5,34 @@
 
 #include <fstream>
 
-namespace joedb {
-TEST(ui, client) {
-  const std::array<const char *, 5> args{"--check", "none", "--db",
-                                         "interpreted", "memory"};
+namespace joedb
+{
+ TEST(ui, client)
+ {
+  const std::array<const char *, 5> args
+  {
+   "--check",
+   "none",
+   "--db",
+   "interpreted",
+   "memory"
+  };
 
   Arguments arguments(args.size(), args.data());
 
-  Client_Parser parser(Open_Mode::shared_write, Client_Parser::DB_Type::none,
-                       arguments);
+  Client_Parser parser
+  (
+   Open_Mode::shared_write,
+   Client_Parser::DB_Type::none,
+   arguments
+  );
 
   {
-    std::ostringstream out;
-    parser.print_help(out);
+   std::ostringstream out;
+   parser.print_help(out);
   }
 
-  Writable_Client *writable_client =
-      dynamic_cast<Writable_Client *>(parser.get());
+  auto *writable_client = dynamic_cast<Writable_Client *>(parser.get());
   ASSERT_TRUE(writable_client);
   Writable_Client_Command_Processor processor(*writable_client);
 
@@ -37,22 +48,34 @@ TEST(ui, client) {
   reference_string << reference_file.rdbuf();
 
   EXPECT_EQ(reference_string.str(), out.str());
-}
+ }
 
-TEST(ui, file_client) {
-  const std::array<const char *, 8> args{
-      "--check",    "none", "--db",        "interpreted",
-      "test.joedb", "file", "interpreted", "connection_file.joedbi"};
+ TEST(ui, file_client)
+ {
+  const std::array<const char *, 8> args
+  {
+   "--check",
+   "none",
+   "--db",
+   "interpreted",
+   "test.joedb",
+   "file",
+   "interpreted",
+   "connection_file.joedbi"
+  };
 
   Arguments arguments(args.size(), args.data());
 
-  Client_Parser parser(Open_Mode::shared_write, Client_Parser::DB_Type::none,
-                       arguments);
+  Client_Parser parser
+  (
+   Open_Mode::shared_write,
+   Client_Parser::DB_Type::none,
+   arguments
+  );
 
   ASSERT_TRUE(parser.has_file());
 
-  Writable_Client *writable_client =
-      dynamic_cast<Writable_Client *>(parser.get());
+  auto *writable_client = dynamic_cast<Writable_Client *>(parser.get());
   ASSERT_TRUE(writable_client);
   Writable_Client_Command_Processor processor(*writable_client);
 
@@ -74,15 +97,19 @@ TEST(ui, file_client) {
   std::ostringstream file_connection_string;
   file_connection_string << file_connection.rdbuf();
 
-  ASSERT_EQ(file_connection_string.str(), "create_table person\n"
-                                          "add_field person name string\n"
-                                          "insert_into person 0\n"
-                                          "update person 0 name \"toto\"\n"
-                                          "insert_into person 1\n"
-                                          "update person 1 name \"titi\"\n"
-                                          "\n");
+  ASSERT_EQ
+  (
+   file_connection_string.str(),
+   "create_table person\n"
+   "add_field person name string\n"
+   "insert_into person 0\n"
+   "update person 0 name \"toto\"\n"
+   "insert_into person 1\n"
+   "update person 1 name \"titi\"\n"
+   "\n"
+  );
 
   std::remove(args[4]);
   std::remove(args[7]);
+ }
 }
-} // namespace joedb
