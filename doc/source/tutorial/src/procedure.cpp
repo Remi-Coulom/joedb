516b774672775457a3479644f18b7e2aa673898b
Rémi Coulom <remi.coulom@gmail.com>
2025-06-07 21:41:26 +0200

    better naming

--- doc/source/tutorial/src/procedure.cpp
+++ doc/source/tutorial/src/procedure.cpp
@@ -8,13 +8,11 @@ static int procedure(joedb::Arguments &arguments)
 {
  arguments.add_parameter("<city_name>*");
 
- if (arguments.size() < 2)
+ if (arguments.size() == 1)
  {
   arguments.print_help(std::cerr);
   return 1;
  }
-
- tutorial::File_Client client("tutorial.joedb");
 
  tutorial::procedures::get_population::Memory_Database get_population;
 
@@ -27,17 +25,19 @@ static int procedure(joedb::Arguments &arguments)
   );
  }
 
- tutorial::procedures::execute(client, get_population);
+ {
+  tutorial::File_Client client("tutorial.joedb");
+  tutorial::procedures::execute(client, get_population);
+  get_population.soft_checkpoint();
+ }
 
  tutorial::procedures::get_population::Readable readable(get_population);
  joedb::Readable_Command_Processor processor(readable);
  processor.print_table
  (
   std::cout,
-  tutorial::procedures::get_population::interpreted_data::table_id
+  tutorial::procedures::get_population::data_table::id
  );
-
- get_population.soft_checkpoint();
 
  return 0;
 }
