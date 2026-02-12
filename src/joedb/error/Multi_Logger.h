38ddb72c8166fe1b82eded5293498e75b3528ee2
Rémi Coulom <remi.coulom@gmail.com>
2026-02-12 15:44:55 +0100

    Multi_Logger

---
+++ src/joedb/error/Multi_Logger.h
@@ -0,0 +1,35 @@
+#ifndef joedb_Multi_Logger_declared
+#define joedb_Multi_Logger_declared
+
+#include "joedb/error/Logger.h"
+
+#include <vector>
+#include <memory>
+
+namespace joedb
+{
+ class Multi_Logger: public Logger
+ {
+  private:
+   std::vector<std::unique_ptr<Logger>> loggers;
+
+  public:
+   void log(const std::string &message) noexcept override
+   {
+    for (const auto &logger: loggers)
+     logger->log(message);
+   }
+
+   void clear() 
+   {
+    loggers.clear();
+   }
+
+   void add(std::unique_ptr<Logger> logger)
+   {
+    loggers.emplace_back(std::move(logger));
+   }
+ };
+}
+
+#endif
