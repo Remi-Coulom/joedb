800757cf2cc84e2063987074967963ba26f8d336
Rémi Coulom <remi.coulom@gmail.com>
2026-05-25 19:49:41 +0200

    no special case for object libraries (necessary for crazy_zero in msvc)

--- compcmake/joedbc.cmake
+++ compcmake/joedbc.cmake
@@ -8,10 +8,7 @@ function(target_uses_joedb target)
  add_dependencies(${target} all_joedbc)
  target_include_directories(${target} PUBLIC ${JOEDB_DIR}/../src)
  get_target_property(target_type ${target} TYPE)
-
- if (NOT "${target_type}" STREQUAL "OBJECT_LIBRARY")
-  target_link_libraries(${target} joedb ${JOEDB_EXTERNAL_LIBS})
- endif()
+ target_link_libraries(${target} joedb ${JOEDB_EXTERNAL_LIBS})
 endfunction()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
