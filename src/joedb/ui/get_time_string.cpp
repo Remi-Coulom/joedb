91611f9f9e59a50821b89f6eaac540679d72c38e
Rémi Coulom <remi.coulom@gmail.com>
2026-09-03 23:50:35 +0200

    safe get_time_string

--- src/joedb/ui/get_time_string.cpp
+++ src/joedb/ui/get_time_string.cpp
@@ -8,18 +8,29 @@ namespace joedb
  std::string get_time_string(int64_t timestamp)
  /////////////////////////////////////////////////////////////////////////////
  {
-  constexpr size_t buffer_size = 24;
-  char buffer[buffer_size];
   const time_t stamp = time_t(timestamp);
   struct tm tm;
-#if defined(__unix__)
-  gmtime_r(&stamp, &tm);
-#else
-  tm = *std::gmtime(&stamp);
-#endif
-  std::strftime(buffer, buffer_size, "%Y-%m-%d %H:%M:%S GMT", &tm);
 
-  return std::string(buffer);
+#ifdef _WIN32
+  if (gmtime_s(&tm, &stamp) != 0)
+#else
+  if (gmtime_r(&stamp, &tm) == nullptr)
+#endif
+   return "bad timestamp";
+
+  constexpr size_t buffer_size = 64;
+  std::string result(buffer_size, '\0');
+
+  const size_t size = std::strftime
+  (
+   result.data(),
+   result.size(),
+   "%Y-%m-%d %H:%M:%S GMT",
+   &tm
+  );
+
+  result.resize(size);
+  return result;
  }
 
  /////////////////////////////////////////////////////////////////////////////
